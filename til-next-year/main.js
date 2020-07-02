9a4e4578e112b1510409602e3afdc3900a99512a
Dain Kim <dev.dain.k@gmail.com>
2020-07-02 13:33:50 +0900

    파일 업로드 삭제, url 방식으로 대체

--- til-next-year/main.js
+++ til-next-year/main.js
@@ -1,3 +1,9 @@
+const customPath = document.querySelector('.custom-path');
+const range = document.querySelector('.range');
+const wrap = document.getElementById('wrap');
+const overlay = document.querySelector('.overlay');
+const source = document.querySelector('.source');
+
 function clock() {
   const content = document.querySelector('h1');
   content.innerHTML = '';
@@ -10,35 +16,43 @@ function clock() {
   let s = (m - parseInt(m)) * 60;
 
   content.innerHTML = 
-    `${parseInt(diff / (60 * 60 * 24 * 1000))} 일 ${parseInt((day - parseInt(day)) * 24)} 시간  ` +
+    `${parseInt(diff / (60 * 60 * 24 * 1000)) + 1} 일 ${parseInt((day - parseInt(day)) * 24)} 시간  ` +
     `${parseInt((h - parseInt(h)) * 60)} 분  ${parseInt((m - parseInt(m)) * 60)} 초  `;
 }
 
-function displayBGImg(overlay, range) {
-  let rangeVal = parseFloat(localStorage.getItem('range'));
+function displayBGImg() {
+  let rangeVal;
+  let pathVal;
   if (localStorage.getItem('range')) {
+    rangeVal = parseFloat(localStorage.getItem('range'));
     overlay.style.backgroundColor = `rgba(0, 0, 0, ${rangeVal})`;
     range.value = rangeVal * 25;
+  }
+  if (localStorage.getItem('path')) {
+    pathVal = localStorage.getItem('path');
+    wrap.style.backgroundImage = `url('${pathVal}')`;
+    customPath.value = pathVal;
+    source.style.display = 'none';
+  } else {
+    wrap.style.backgroundImage = `url('https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')`;
+    source.style.display = 'block';
   }
 }
 
 function start() {
-  const fileInput = document.querySelector('.file-upload');
-  const range = document.querySelector('.range');
-  const wrap = document.getElementById('wrap');
-  const overlay = document.querySelector('.overlay');
-  const source = document.querySelector('.source');
   clock();
   setInterval(clock, 1000);
-  displayBGImg(overlay, range);
+  displayBGImg(overlay, range, wrap);
   
-  fileInput.addEventListener('change', function(e) {
-    try {
-      let tempPath = URL.createObjectURL(e.target.files[0]);
-      wrap.style.backgroundImage = `url('${tempPath}')`;
+  customPath.addEventListener('change', function(e) {
+    if (customPath.value.trim() === '') {
+      wrap.style.backgroundImage = `url('https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')`;
+      source.style.display = 'block';
+      localStorage.removeItem('path');
+    } else {
+      wrap.style.backgroundImage = `url('${customPath.value.trim()}')`;
       source.style.display = 'none';
-    } catch {
-      console.log('error!');
+      localStorage.setItem('path', customPath.value.trim());
     }
   });
   range.addEventListener('change', function() {
